1911d03ac74072484fc72fb2c46f7797c0eecad7
matias erenchun <matiaserenchun@gmail.com>
2018-11-27 17:34:17 -0300

    clase lector
    se creo la clase lector  y el método leer contraseña

---
+++ src/Lector.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Lector
+{
+    private Scanner lector;
+    public Lector()
+    {
+        this.lector = new Scanner(System.in);
+    }
+
+    public String leerContraseña()
+    {
+        String contraseña = this.lector.nextLine();
+        while(contraseña.equals("")|| contraseña.equals(null)|| contraseña.length()<6)
+        {
+            System.out.println(" contraseña muy corta, por favor ingrese una con mas de 6 caracteres");
+            contraseña = this.lector.nextLine();
+        }
+        return contraseña;
+    }
+
+}
