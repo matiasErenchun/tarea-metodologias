e0744b84de3d36b052c730b4e9ea79abe350540a
matias erenchun <matiaserenchun@gmail.com>
2018-11-28 14:05:54 -0300

    pruebas de como crear al usuario

--- src/Sistema.java
+++ src/Sistema.java
@@ -1,5 +1,16 @@
 
 public class Sistema
 {
+    public Sistema() {
+    }
 
+    public void menu()
+    {
+
+    }
+    public Usuario_Participante crearCuenta()
+    {
+        Vendedor vendedor= new Vendedor("hola","1234",01);
+        return  vendedor;
+    }
 }
