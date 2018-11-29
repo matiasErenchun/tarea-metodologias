ef43a8c4484927ef4863a1414c771bbf61510ca2
matias erenchun <matiaserenchun@gmail.com>
2018-11-28 23:15:28 -0300

    auns e trabaja en crear las cuentas

--- src/Lector.java
+++ src/Lector.java
@@ -19,4 +19,26 @@ public class Lector
         return contraseña;
     }
 
+    public Integer leerNumero(String mensaje)
+    {
+        System.out.println(mensaje);
+        String  numero = this.lector.nextLine();
+        boolean seguir = true;
+        while (seguir)
+        {
+            if(numero.matches("^[0-9]+$"))
+            {
+                seguir=false;
+            }
+            else
+            {
+                System.out.println("error en los campos ingresados por favor siga las instrucciones");
+                System.out.println(mensaje);
+                numero = this.lector.nextLine();
+            }
+
+        }
+        return Integer.parseInt(numero);
+    }
+
 }
--- src/Sistema.java
+++ src/Sistema.java
@@ -1,16 +1,67 @@
+import java.util.ArrayList;
 
 public class Sistema
 {
-    public Sistema() {
+    private Lector lector;
+    private Integer id;
+    //private falta el hash set de compradores y vendedores
+    public Sistema()
+    {
+        this.lector = new Lector();
+        this.id=1;
     }
 
-    public void menu()
+    public void menuPrincipal()
     {
+        int numero;
+        boolean seguir =true;
+        while(seguir)
+        {
+            this.mostrarMenuPrincipal();
+            numero = this.lector.leerNumero("ingrese el numero de la opcion que dese ejecutar");
+            switch (numero) {
+                case 0:
+                    seguir=false;
+                    System.out.println(" sitema lo regresara ala pantalla principal, gracias.");
+                    break;
+                case 1:
 
+                    break;
+                default:
+                    System.out.println("opcion no valida ingrese una nueva que si sea valida");
+                    break;
+            }
+        }
     }
-    public Usuario_Participante crearCuenta()
-    {
-        Vendedor vendedor= new Vendedor("hola","1234",01);
-        return  vendedor;
-    }
+   public void mostrarMenuPrincipal()
+   {
+       System.out.println("ingrese 1 si desea crear una cuenta");
+       System.out.println("ingrese 0 si desea salir del sistema");
+   }
+
+   public Usuario_Participante crearCuenta()
+   {
+        int numero = this.lector.leerNumero("ingrese 1 para cuenta de vendedro, 2 para cuenat de comprador");
+
+         if(numero==1)
+         {
+             String nombre = this.lector.leerContraseña();
+             String contraseña = this.lector.leerContraseña();
+             Vendedor vendedor = new Vendedor(nombre, contraseña, this.id);
+             this.id++;
+             return vendedor;
+             //falta agregar al has set
+         }
+         else
+             {
+                String nombre=this.lector.leerContraseña();
+                String contraseña=this.lector.leerContraseña();
+                Comprador comprador=new Comprador(nombre,contraseña,this.id);
+                this.id++;
+                return comprador;
+             }
+
+
+   }
+
 }
